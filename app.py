e6824f26966e04f1adb43ace1e8cdf8bbd47c3cf
vdesyatke <72273263+vdesyatke@users.noreply.github.com>
2023-10-23 11:55:13 +0400

    Added models (decision tree and min max scaler)

--- app.py
+++ app.py
@@ -2,8 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def print_hello():
+@app.route('/', methods=['get', 'post'])
+def predict():
+    if request.method == 'POST':
+        i_w = request.form.get('IW')
+        i_f = request.form.get('IF')
+        v_w = request.form.get('VW')
+        f_p = request.form.get('FP')
+
+        inp = np.array([[float(i_w),float(i_f), float(v_w), float(f_p)]])
+
+        with open("/models/welding_seam_model.pkl", "rb") as f:
+            model = pickle.load(f)
+
+        with open("/models/welding_seam_scaler.pkl", "rb") as f:
+            scaler = pickle.load(f)
+
+        inp = scaler.transform(inp)
+        predict = model.predict(inp)
+
     return render_template('index.html')
 
 
