a98cb7e65ae284c3c997c35d5f497fc438f17f68
vdesyatke <72273263+vdesyatke@users.noreply.github.com>
2023-10-23 17:33:33 +0400

    Removed sklearn and numpy from requirements.txt

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import pickle
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['get', 'post'])
 def predict():
+    message = ''
     if request.method == 'POST':
         i_w = request.form.get('IW')
         i_f = request.form.get('IF')
@@ -25,13 +27,16 @@ def predict():
 
         inp = scaler.transform(inp)
         pred = model.predict(inp)
-        print(pred)
+        # print(pred)
+        message = f'При заданных параметрах сварки глубина и ширина сварного шва составит {pred[0][0]}, {pred[0][1]}'
+        print(message)
 
-    return render_template('index.html')
+    return render_template('index.html', message=message)
 
 
 @app.route('/text/')
 def print_text():
     return '<h1>Some text</h1'
 
-# app.run()
+
+app.run()
